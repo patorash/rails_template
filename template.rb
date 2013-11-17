0168ef09d5ce3609c1bbf939900afe6a54378403
Makoto Henmi <mako.mail.g@gmail.com>
2013-11-17 11:04:34 +0900

    fixedの時だけpadding-top:60pxを追加するように修正

--- template.rb
+++ template.rb
@@ -83,11 +83,11 @@ if use_bootstrap
     generate 'bootstrap:layout', 'application fluid'
   else
     generate 'bootstrap:layout', 'application fixed'
+    append_to_file 'app/assets/stylesheets/application.css' do
+      "body { padding-top:60px }"
+    end
   end
   remove_file 'app/views/layouts/application.html.erb'
-  append_to_file 'app/assets/stylesheets/application.css' do
-    "body { padding-top:60px }"
-  end
 else
   generate 'simple_form:install'
 end
